da309755fa2320b4e3dd5ecb77dd4d39d17902cd
YeJi Kim <2214yjgm@gmail.com>
2023-03-11 03:29:50 +0900

    [Silver II] Title: 최대 힙, Time: 352 ms, Memory: 26608 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,21 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2) -> o2 - o1);
         for(int i = 0; i < N; i++){
             int op = Integer.parseInt(br.readLine());
             if(op == 0){
-                if(queue.isEmpty()) System.out.println(0);
-                else System.out.println(queue.poll());
+                if(queue.isEmpty()) bw.write("0\n");
+                else bw.write(queue.poll() + "\n");
             }else{
                 queue.offer(op);
             }
         }
+        bw.flush();
     }
 }
