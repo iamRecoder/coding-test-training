d3d0b01e4a5df4ba0447e1ca00edebf2188bec00
YeJi Kim <2214yjgm@gmail.com>
2023-03-15 03:37:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1212 ms, Memory: 183796 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            map.put(input, map.getOrDefault(input, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(input, 0) + " ");
+        }
+        System.out.println(sb);
+
+    }
+}
