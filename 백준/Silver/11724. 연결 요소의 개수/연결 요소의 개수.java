795cf9786bc1516cc571f85ccfa60ecf70af4ac0
YeJi Kim <2214yjgm@gmail.com>
2023-03-16 03:27:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 141416 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,43 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main {
-  static ArrayList<Integer>[] A;
-  static boolean visited[];
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    A = new ArrayList[n + 1];
-    visited = new boolean[n + 1];
-    for (int i = 1; i < n + 1; i++) { // 인접 리스트 초기화
-      A[i] = new ArrayList<Integer>();
+    static boolean[] visited;
+    static ArrayList<Integer>[] adj;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        adj = new ArrayList[N + 1];
+
+        for (int i = 1; i < N + 1; i++) { // 인접 리스트 초기화
+            adj[i] = new ArrayList<Integer>();
+        }
+
+        for (int k = 0; k < M; k++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            adj[s].add(e);
+            adj[e].add(s);
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if(!visited[i]){
+                cnt++;
+                dfs(i);
+            }
+        }
+
+        System.out.println(cnt);
+
     }
-    for (int i = 0; i < m; i++) {
-      st = new StringTokenizer(br.readLine());
-      int s = Integer.parseInt(st.nextToken());
-      int e = Integer.parseInt(st.nextToken());
-      A[s].add(e); // 양 방향 간선이므로 양쪽으로 간선을 더 해준다
-      A[e].add(s);
+
+    static void dfs(int i) {
+        if(visited[i]){
+            return;
+        }else{
+            visited[i] = true;
+            for(int j: adj[i]){
+                if(visited[j] == false)
+                    dfs(j);
+            }
+        }
+
     }
-    int count = 0;
-    for (int i = 1; i < n + 1; i++) {
-      if (!visited[i]) { // 미 방문한 정점이 없을 때까지 반복
-        count++;
-        DFS(i);
-      }
-    }
-    System.out.println(count);
-  }
-  static void DFS(int v) {
-    if (visited[v]) {
-      return;
-    }
-    visited[v] = true;
-    for (int i : A[v]) {
-      if (visited[i] == false) { // 연결 정점 중 방문하지 않았던 정점만 탐색함
-        DFS(i);
-      }
-    }
-  }
 }
