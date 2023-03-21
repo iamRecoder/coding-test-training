2cf5f8e46301647310271bd7c9dfb24517076dc3
YeJi Kim <2214yjgm@gmail.com>
2023-03-22 02:06:52 +0900

    [Silver I] Title: 최대공약수, Time: 256 ms, Memory: 20248 KB -BaekjoonHub

--- 백준/Silver/1850. 최대공약수/최대공약수.java
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -20,8 +17,7 @@ public class Main {
             mod = min % mod;
             min = tmp;
         }
-        StringBuilder sb = new StringBuilder();
-        LongStream.range(0, min).forEach(i -> sb.append("1"));
-        System.out.println(sb);
+       
+        System.out.println("1".repeat((int)min));
     }
 }
