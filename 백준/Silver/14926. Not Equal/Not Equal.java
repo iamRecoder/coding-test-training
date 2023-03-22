a683cdb5a28d2e583f6ffd2fac0bacb144451b63
YeJi Kim <2214yjgm@gmail.com>
2023-03-23 02:43:12 +0900

    [Silver I] Title: Not Equal, Time: 1112 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/14926. Not Equal/Not Equal.java
+++ 백준/Silver/14926. Not Equal/Not Equal.java
@@ -5,18 +5,15 @@ public class Main {
         try {
             Scanner sc = new Scanner(System.in);
             int N = sc.nextInt();
-            int[][] edges = new int[N + 1][];
             boolean[][] visited = new boolean[N + 1][];
             for (int i = 0; i <= N; i++) {
-                edges[i] = new int[N + 1];
                 visited[i] = new boolean[N + 1];
             }
 
             int currentVertex = 0;
-
             int forLoopCount = (N) * (N - 1) / 2;
             visited[1][N] = visited[N][1] = true;
-            while (forLoopCount > 0) {
+            while (forLoopCount-- > 0) {
                 for (int i = 1; i <= N; i++) {
                     if (i == currentVertex || visited[currentVertex][i]) continue;
                     visited[currentVertex][i] = visited[i][currentVertex] = true;
@@ -24,7 +21,6 @@ public class Main {
                     break;
                 }
                 System.out.print("a" + currentVertex + " ");
-                forLoopCount--;
             }
             System.out.print("a1");
         }catch (Exception e){
