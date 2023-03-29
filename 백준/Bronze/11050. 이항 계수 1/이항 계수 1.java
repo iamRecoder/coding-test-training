ba3aad44f09add5c9c898466056da8fa0a66a5b5
YeJi Kim <2214yjgm@gmail.com>
2023-03-30 02:40:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int nPk = IntStream.rangeClosed((n-k+1),n).reduce(1,(i,j) -> i * j);
-        int r = IntStream.rangeClosed(1,k).reduce(1, (i,j) -> i * j);
-        System.out.println(nPk/r);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int nPk = IntStream.rangeClosed((n - k + 1), n).reduce(1, (i, j) -> i * j);
+        int r = IntStream.rangeClosed(1, k).reduce(1, (i, j) -> i * j);
+        System.out.println(nPk / r);
     }
 }
