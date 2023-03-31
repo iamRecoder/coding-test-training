a5ec24fc2475e3540b935bd4deec13acc9a531f9
YeJi Kim <2214yjgm@gmail.com>
2023-04-01 01:57:25 +0900

    [Silver I] Title: 스티커, Time: 708 ms, Memory: 136472 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -5,37 +5,30 @@ import java.util.Arrays;
 
 public class Main {
     static int[][] stickers;
-    static int[][] dp, visited;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int T = Integer.parseInt(br.readLine());
-            for (int i = 0; i < T; i++) {
-                // 값 입력받기
-                int N = Integer.parseInt(br.readLine());
-                stickers = new int[2][N];
-                stickers[0] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                stickers[1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                dp = new int[2][N]; // i번째가 채워진 경우
-                visited = new int[2][N];
-                int max = Integer.MIN_VALUE;
-                calc(N);
-                System.out.println(Math.max(dp[0][N-1],dp[1][N-1]));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            // 값 입력받기
+            int N = Integer.parseInt(br.readLine());
+            stickers = new int[2][N];
+            stickers[0] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            stickers[1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            dp = new int[2][N];
+            // 자기 위치를 선택했을 때 최대값을 갖게 되는 dp 배열 구하기
+            calc(N);
+            System.out.println(Math.max(dp[0][N - 1], dp[1][N - 1]));
+        }
     }
 
     static void calc(int N) {
         for (int j = 0; j < N; j++) {
             if (j == 0) {
                 dp[0][j] = stickers[0][0];
-//                visited[0][j] = 0;
                 dp[1][j] = stickers[1][0];
-//                visited[1][j] = 1;
             } else if (j == 1) {
                 dp[1][j] = dp[0][j - 1] + stickers[1][j];
                 dp[0][j] = dp[1][j - 1] + stickers[0][j];
