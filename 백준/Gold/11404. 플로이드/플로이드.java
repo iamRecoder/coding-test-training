4340c407a271b41f3e247862c2422b6a83bd1c7e
YeJi Kim <2214yjgm@gmail.com>
2023-04-06 11:11:01 +0900

    [Gold IV] Title: 플로이드, Time: 644 ms, Memory: 44308 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,43 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
-  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int N, M;
-  static int distance[][];
-  public static void main(String[] args) throws IOException {
-    N = Integer.parseInt(br.readLine());
-    M = Integer.parseInt(br.readLine());
-    distance = new int[N + 1][N + 1];
-    for (int i = 1; i <= N; i++) { // 인접 행렬 초기화
-      for (int j = 1; j <= N; j++) {
-        if (i == j)
-          distance[i][j] = 0;
-        else
-          distance[i][j] = 10000001; // 충분히 큰수로 저장
-      }
-    }
-    for (int i = 0; i < M; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int s = Integer.parseInt(st.nextToken());
-      int e = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
-      if (distance[s][e] > v) distance[s][e] = v;
-    }
-    for (int k = 1; k <= N; k++) { // 플로이드 워셜 알고리즘 수행
-      for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-          if (distance[i][j] > distance[i][k] + distance[k][j])
-            distance[i][j] = distance[i][k] + distance[k][j];
+    public static void main(String[] args) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
+            StringTokenizer st;
+
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(arr[i], 10000001);
+                arr[i][i] = 0;
+            }
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken()) - 1;
+                int end = Integer.parseInt(st.nextToken()) - 1;
+                int cost = Integer.parseInt(st.nextToken());
+                arr[start][end] = Math.min(arr[start][end], cost);
+            }
+
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                    }
+                }
+            }
+
+            for (int[] a : arr) {
+                for (int e : a) {
+                    if (e == 10000001) System.out.print(0 + " ");
+                    else System.out.print(e + " ");
+                }
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-      }
     }
-    for (int i = 1; i <= N; i++) {
-      for (int j = 1; j <= N; j++) {
-        if (distance[i][j] == 10000001) System.out.print("0 ");
-        else System.out.print(distance[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
+
 }
+
+
