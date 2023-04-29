8a1257f838ce50ac11d8ba4ca51a6db587c1f029
YeJi Kim <2214yjgm@gmail.com>
2023-04-29 22:58:52 +0900

    [Bronze I] Title: 평균, Time: 148 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,19 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] score = new int[n];
-        int max = 0;
-        int sum = 0;
-        for(int i = 0; i<n; i++){
-            score[i] = sc.nextInt();
-            if(score[i]> max){
-                max = score[i];
-            }
-            sum += score[i];
-        }
-        System.out.println(sum*100.0/n/max);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = Arrays.stream(arr).max().getAsInt();
+        double avg = Arrays.stream(arr).average().getAsDouble() / max * 100;
+        System.out.println(avg);
     }
 }
