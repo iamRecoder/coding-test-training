5304a56707c541f96c60842f9ae9c511b487fa90
YeJi Kim <2214yjgm@gmail.com>
2023-04-29 23:08:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1456 ms, Memory: 58576 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,23 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-    int suNo = Integer.parseInt(stringTokenizer.nextToken());
-    int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-    long[] S = new long[suNo + 1];
-    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-    
-    for (int i = 1; i <= suNo; i++) {
-      S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] sum = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            System.out.println(sum[e] - sum[s - 1]);
+        }
+
     }
-      
-    for (int q = 0; q < quizNo; q++) {
-      stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-      int i = Integer.parseInt(stringTokenizer.nextToken());
-      int j = Integer.parseInt(stringTokenizer.nextToken());
-      System.out.println(S[j] - S[i - 1]);
-    }
-  }
 }
