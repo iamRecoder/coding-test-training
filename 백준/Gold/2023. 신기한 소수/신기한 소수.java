e88193bf7cc989d22992746406763504b39de534
김예지 <2214yjgm@gmail.com>
2024-10-04 21:01:23 +0900

    [Gold V] Title: 신기한 소수, Time: 408 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        DFS(2,1);
+        DFS(3,1);
+        DFS(5,1);
+        DFS(7,1);
+    }
+
+    public static void DFS(int num, int jarisu){
+        if(isPrime(num)) {
+            if (jarisu == N) {
+                System.out.println(num);
+            } else {
+                for (int i = 1; i < 10; i+=2) {
+                    DFS(num * 10 + i, jarisu + 1);
+                }
+            }
+        }
+    }
+
+
+    public static boolean isPrime(int num){
+        for(int i = 2; i < num/2; i++){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
